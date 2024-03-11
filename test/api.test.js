93e9558d779148cb18f1e67f5680d06dd8f71188
seixass <138785505+seixass@users.noreply.github.com>
2024-03-11 12:25:48 -0300

    Update api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,3 +1,5 @@
+// 1º metodo
+
 const axios = require ('axios')
 
 async function ApiDogs(){
@@ -10,3 +12,18 @@ test('Verificando se obtém os dados corretamente',async () =>{
     const dados = await ApiDogs();
     expect(dados).toBeDefined();
 }) 
+
+// 2º metodo
+
+const axios = require('axios')
+
+async function Absolutylb(){
+    const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=maceio&units=metric&appid=8c21584a19c15bb0b57fccb61795c2c1&lang=pt_br')
+   
+    return response.data
+}
+
+test('Verifica se obtém os dados corretamento', async () => {
+    const data = await Absolutylb();
+    expect(data).toBeDefined();
+})
